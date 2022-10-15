2660d0570eecc0ad12182e4b8428d5415bd61997
Emeka-code <clintonekegbu@gmail.com>
2022-10-16 00:25:06 +0100

    fashion

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Body from "./Body/Body";
+import Cover from "./Body/Cover";
+import Header from "./Header/Header";
+
+const App = () => {
+  return (
+    <div>
+      {/* <Header /> */}
+      {/* <Body /> */}
+      <Cover />
+    </div>
+  );
+};
+
+export default App;
